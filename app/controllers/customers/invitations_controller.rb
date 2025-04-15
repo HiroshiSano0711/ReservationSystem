16d6a34d5e035960e316358cf539a00560328535
HiroshiSano0711 <hiroshi.sano0711@gmail.com>
2025-04-15 22:17:04 +0900

    予約完了時に新規登録したあと予約が紐づかないバグの修正

--- app/controllers/customers/invitations_controller.rb
+++ app/controllers/customers/invitations_controller.rb
@@ -2,12 +2,31 @@
 
 class Customers::InvitationsController < Devise::InvitationsController
   def update
-    super
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
 
-    reservation_session = Reservations::SessionData.new(session)
-    if reservation_session.public_id.present?
-      ReservationLinker.new(customer: resource, public_id: reservation_session.public_id).link_reservation
-      reservation_session.clear_public_id
+    yield resource if block_given?
+
+    if invitation_accepted
+      reservation_session = Reservations::SessionData.new(session)
+      if reservation_session.public_id.present?
+        ReservationLinker.new(customer: resource, public_id: reservation_session.public_id).link_reservation
+        reservation_session.clear_public_id
+      end
+      if resource.class.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+        respond_with resource, location: after_accept_path_for(resource)
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, location: new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with(resource)
     end
   end
 end
